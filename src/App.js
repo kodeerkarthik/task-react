614fc12397d5682b341d1bf966cb8cfd142474f9
kodderkarthik <kkrishnegowdaostb1@gmail.com>
2019-06-12 14:42:06 +0530

    task3 completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom
 import './App.css';
 import Agecls from './Task2/Agecls';
 import Signup from './Task1/Signup';
+import Arithmatic from './Task3/Arithmatic';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route exact path='/task1' component={Signup}></Route>
           <Route exact path='/task2'component={Agecls}></Route>
+          <Route exact path='/task3'component={Arithmatic}></Route>
          </Switch>
       </Router>
     </div>
---
+++ src/Task3/Arithmatic.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './Arithmatic.css';
+class Arithmatic extends Component {
+    constructor(props){
+    super(props);
+    this.state={
+        add: 10,
+        sub: 10,
+        mul: 10
+    };
+}
+addition=()=>{
+    this.setState(
+        {add:this.state.add =this.state.add + 5}
+    )
+}
+substraction=()=>{
+    this.setState(
+        {sub:this.state.sub =this.state.sub - 5}
+    )
+}
+multiplication=()=>{
+    this.setState(
+        {mul:this.state.mul =this.state.mul * 5}
+    )
+}
+    render() {
+        return (
+            <div>
+                <b>{this.state.add}</b>                
+                <b>{this.state.sub}</b>
+                <b>{this.state.mul}</b><br></br>
+                <button onClick={this.addition}>Addition</button>
+                <button onClick={this.substraction}>Substraction</button>
+                <button onClick={this.multiplication}>Multiplication</button>
+            </div>
+        );
+    }
+}
+
+export default Arithmatic;
